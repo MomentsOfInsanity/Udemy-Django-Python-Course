bd44052a2572b22ed098c48f4711c3a992784230
Raj Bedi <Raj.j.bedi@gmail.com>
2021-11-23 12:43:11 -0500

    Added While Loops HTML and JS

---
+++ JS_LEVEL_ONE/whileLoops.js
@@ -0,0 +1,20 @@
+var num = 1;
+// Write a while loop that prints out
+// only the even numbers from 1 to 10
+
+while (num < 11) {
+  //console.log("x is currently: " + num);
+  if (num%2 === 0) {
+    console.log(num);
+  }
+  num = num+1;
+
+  // if (x===3) {
+  //   console.log("X IS EQUAL TO THREE!");
+  //   break;
+  // }
+
+  // console.log("x is still less than 5, adding 1 to x");
+
+  // x = x+2;
+}
