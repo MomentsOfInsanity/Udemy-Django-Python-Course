ea2fee2ce0a1073c56fea3475b272003b71f2c29
Raj Bedi <Raj.j.bedi@gmail.com>
2021-11-23 13:03:36 -0500

    For Loops HTMl/JS

---
+++ JS_LEVEL_ONE/forLoops.js
@@ -0,0 +1,11 @@
+// For Loops
+
+// for (var i = 0; i < 5; i++) {
+//   console.log("Number is " +i);
+// }
+
+var word = "ABCDEFGHIJK"
+
+for (var i = 0; i < word.length; i=i+2) {
+  console.log(word[i]);
+}
