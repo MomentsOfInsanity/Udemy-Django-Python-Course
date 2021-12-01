51f2169c864935884d1f2b9c47d7ddacf9508c15
Raj Bedi <Raj.j.bedi@gmail.com>
2021-12-01 17:31:23 -0500

    Part 5 Objects Continued

--- JS_LEVEL_TWO/Part5Objects.js
+++ JS_LEVEL_TWO/Part5Objects.js
@@ -1,3 +1,4 @@
+//js console
 var carInfo = {make:"Toyota",year:1990,model:"Camry"};
 // carInfo
 
@@ -7,4 +8,19 @@ var myNewO = {a:"hello",b:[1,2,3],c:{inside:['a','b']}};
 for (k in carInfo){
   console.log(k);
   console.log(carInfo[k]);
+}
+
+var simple = {
+  prop : "Hello",
+  myMethod : function(){
+    console.log("The myMethod was called");
+  }
+}
+simple.myMethod
+
+var myObj = {
+  name : "Jose",
+  greet: function(){
+    console.log("Hello " + this.name);
+  }
 }
